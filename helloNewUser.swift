5c586c93ae17d4fa935798ab287eb7a15012a376
renatAQE <renatcbr@mail.ru>
2025-07-29 15:29:25 +0300

    first

---
+++ helloNewUser.swift
@@ -0,0 +1,14 @@
+var name : String = "Renat"//
+var age : int = 30
+var isStudent : bool = true
+var pi : double = 3.14
+
+
+let companyName : String = "Apple"
+
+//  helloNewUser.swift
+//  
+//
+//  Created by Алина Каримова on 22.07.2025.
+//
+
---
+++ hw1.swift
@@ -0,0 +1,18 @@
+var seconds  = 654321654321
+
+var minutes = seconds / 60 // перевод секунд в минуты
+var hours = minutes / 60  // перевод минут в часы
+var days = hours / 24 // перевод часы в дни
+var weeks = days / 7 // перевод дней в недели
+var months = weeks / 30 // перевод недель в месяцы
+var years = months / 12 // перевод месяцев в годы
+
+print("\(seconds) секунд это \(minutes) минут \(hours) часов \(days) дней \(weeks) неделя \(months) месяца \(years) лет!")
+    
+//
+//  hw1.swift
+//  
+//
+//  Created by Алина Каримова on 29.07.2025.
+//
+
